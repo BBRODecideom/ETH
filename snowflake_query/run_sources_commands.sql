bbfb08b8e96442d925058f067539b204a8265afb
Benoît Brossillon <bbrossillon@decideom.fr>
2026-06-26 15:25:05 +0200

    Okai boys we are back. Update package, change snowflake account and try dbt core v2

---
+++ snowflake_query/run_sources_commands.sql
@@ -0,0 +1,6 @@
+-- At first it looks like this :
+SELECT * FROM ETH.ETH_SCHEMA.TOKEN_TRANSFERS
+Limit 100 ;
+-- And we need to change it to this
+select * from DECIDEOM_LYON.ETH_SCHEMA.CONTRACTS limit 40 ;
+
