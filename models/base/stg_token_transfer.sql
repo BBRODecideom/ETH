21e2ec4b8566825365aad82f22e21907f9a6b302
Benoît Brossillon <bbrossillon@decideom.fr>
2026-06-29 15:28:06 +0200

    Change reloaded scope dependings on the target env

--- models/base/stg_token_transfer.sql
+++ models/base/stg_token_transfer.sql
@@ -6,3 +6,13 @@ select
     token_address,
     value
 from {{ source('eth', 'token_transfers') }}
+
+{%- if target.name == 'dev' %}
+
+where date >= dateadd('day', -3, current_date)
+
+{%- elif target.name == 'prod' %}
+
+where date >= dateadd('day', -15, current_date)
+
+{%- endif -%}
